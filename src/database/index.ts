da3fbdd8846d51969009a1928de1d31217198763
Daniel <daniel35contato@gmail.com>
2021-06-16 18:35:36 -0300

    Setup database connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 // Conecta no banco
-mongoose.connect(process.env.MONGODB_HOST || 'mongodb://localhost/initial-db');
+mongoose.connect(process.env.MONGODB_HOST || 'mongodb://localhost/roman-db');
 mongoose.Promise = global.Promise;
 
 export default mongoose;
